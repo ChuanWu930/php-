5ed01ae404a28ee2d9454d6e6df5d9d46f8b880f
ChuanWu930 <107868610+ChuanWu930@users.noreply.github.com>
2024-04-17 22:42:57 +0800

    Update 9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -1,1 +1,14 @@
 
+<?php
+    // 啟動 session
+    session_start();
+
+    // 移除 session 變數 "counter"，即重置計數器
+    unset($_SESSION["counter"]);
+
+    // 顯示計數器重置成功的訊息
+    echo "counter重置成功....";
+
+    // 重新導向至 8.counter.php 頁面，延遲 2 秒後跳轉
+    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
+?>
