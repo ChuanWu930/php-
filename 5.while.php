ff9c31ab3dc11190601d7ebf1f2f107370b6f4b3
ChuanWu930 <107868610+ChuanWu930@users.noreply.github.com>
2024-04-17 22:39:57 +0800

    Update 5.while.php

--- 5.while.php
+++ 5.while.php
@@ -1,1 +1,13 @@
 
+<?php
+   // 使用 mysqli_connect() 函式建立與資料庫的連結
+   $conn = mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+   
+   // 使用 mysqli_query() 函式從資料庫中查詢資料
+   $result = mysqli_query($conn, "select * from user");
+   
+   // 使用 mysqli_fetch_array() 函式從查詢結果中抓取一筆資料，並逐一顯示每筆用戶的帳號和密碼
+   while ($row = mysqli_fetch_array($result)) {
+     echo $row["id"]." ".$row["pwd"]."<br>";
+   } 
+?>        
