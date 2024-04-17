ebfe654c2106012ae6e344184067aaf5a6c4c57e
ChuanWu930 <107868610+ChuanWu930@users.noreply.github.com>
2024-04-17 22:29:02 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,1 +1,22 @@
 
+<html>
+    <head><title>html+php</title></head>      
+    <!-- 頁面標題為 "html+php" -->
+
+    <body>
+        <?php echo "Hello world!"; ?> 
+        <!-- 在網頁中嵌入 PHP 代碼，用於顯示 "Hello world!" 文字 -->
+
+        <h1>html</h1>
+        <!-- 在網頁中插入 HTML 標題 "html" -->
+
+        <?php echo "Hello world!"; ?> 
+        <!-- 再次嵌入 PHP 代碼，用於顯示 "Hello world!" 文字 -->
+
+        <h1>html</h1>
+        <!-- 再次插入 HTML 標題 "html" -->
+
+        <?php echo "Hello world!"; ?> 
+        <!-- 第三次嵌入 PHP 代碼，用於顯示 "Hello world!" 文字 -->
+    </body>
+</html>
